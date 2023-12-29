029b3e624a88b98c4ba417dd7de2396d88ef406d
meeehdi <mehdi.mhiri@gmail.com>
2023-12-29 11:04:13 +0100

    d09: go

--- d09/main.go
+++ d09/main.go
@@ -3,17 +3,10 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
-
-type number struct {
-	value     int
-	adjacents []string
-}
-type symbol struct {
-	value string
-}
 
 func main() {
 	start := time.Now()
@@ -29,6 +22,55 @@ func main() {
 		if line == "" {
 			continue
 		}
+
+		steps := strings.Split(line, " ")
+		diffs := make(map[int][]int)
+		maxLevel := 0
+		for i := 0; i < len(steps)-1; i++ {
+			step, _ := strconv.Atoi(steps[i])
+			nextStep, _ := strconv.Atoi(steps[i+1])
+
+			diff := nextStep - step
+			level := 0
+
+			for {
+				if level > maxLevel {
+					maxLevel = level
+				}
+
+        if diffs[level] == nil {
+          diffs[level] = []int{}
+          diffs[level] = append(diffs[level], diff)
+          break
+        }
+
+				lastDiff := diffs[level][len(diffs[level])-1]
+        diffs[level] = append(diffs[level], diff)
+
+				if lastDiff == 0 && diff == 0 {
+					break
+				} else {
+					diff = diff - lastDiff
+					level++
+				}
+			}
+		}
+
+		diff1 := 0
+		diff2 := 0
+		for i := maxLevel; i >= 0; i-- {
+			levelDiffs := diffs[i]
+			if levelDiffs == nil {
+				continue
+			}
+			diff1 += levelDiffs[len(levelDiffs)-1]
+			diff2 = levelDiffs[0] - diff2
+		}
+
+		lastStep, _ := strconv.Atoi(steps[len(steps)-1])
+		firstStep, _ := strconv.Atoi(steps[0])
+		part1 += lastStep + diff1
+		part2 += firstStep - diff2
 	}
 
 	fmt.Println(part1, part2)
